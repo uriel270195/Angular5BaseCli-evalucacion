6427aa460acbca4ba1e4b16389a4837f0d104725
uriel270195 <uriel_270195@hotmail.com>
2018-05-07 10:46:01 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app'; 
+}
+
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: MainComponent 
+  },
+  { 
+    path: 'about', 
+    component: AboutComponent 
+  },
+  { 
+    path: 'contact', 
+    component: ContactComponent 
+  }
+];
